d07fc856b1c7e38d07f1eb9ee46223072879b277
Marcio Mesquita <botasebatatas@gmail.com>
2022-09-07 11:29:45 -0300

    Aula 3 - movimentando o pacmam com referencia de linhas e colunas

--- pacman/pacman_2.py
+++ pacman/pacman_2.py
@@ -9,10 +9,31 @@ PRETO = (0, 0, 0)
 
 class Pacman:
     def __init__(self):
+        self.coluna = 1
+        self.linha = 1
         self.centro_x = 400
         self.centro_y = 300
-        self.tamanho = 50
+        self.tamanho = 800 // 30
+        self.velocidade_x = 1
+        self.velocidade_y = 1
         self.raio = self.tamanho // 2
+
+    def calcular_regras(self):
+        self.coluna = self.coluna + self.velocidade_x
+        self.linha = self.linha + self.velocidade_y
+        # falta arrumar contato com as bordas da tela
+        self.centro_x = int(self.coluna * self.tamanho + self.raio)
+        self.centro_y = int(self.linha * (self.tamanho + self.raio))
+
+        if self.centro_x + self.raio > 800:
+            self.velocidade_x = -1
+        if self.centro_x - self.raio < 0:
+            self.velocidade_x = 1
+
+        if self.centro_y + self.raio > 600:
+            self.velocidade_y = -1
+        if self.centro_y - self.raio < 0:
+            self.velocidade_y = 1
 
     def pintar(self, tela):
         # desenha o corpo do pacman
@@ -36,10 +57,16 @@ if __name__ == "__main__":
     pacman = Pacman()
 
     while True:
+        # calcula as regras
+        pacman.calcular_regras()
+
         # pinta a tela
+        screen.fill(PRETO)
         pacman.pintar(screen)
         pygame.display.update()
+        pygame.time.delay(100)
 
+        # captura os eventos
         for e in pygame.event.get():
             if e.type == pygame.QUIT:
                 exit()
